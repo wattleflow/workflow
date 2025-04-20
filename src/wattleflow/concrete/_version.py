3d099e49bc2a593e3370e8db4a4ea0af8cff3450
wattleflow <wattleflow@outlook.com>
2025-04-20 10:15:22 +1000

    Updated project version.

--- src/wattleflow/concrete/_version.py
+++ src/wattleflow/concrete/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.0.20.dev2+g3bbb008'
-__version_tuple__ = version_tuple = (0, 0, 0, 20, 'dev2', 'g3bbb008')
+__version__ = version = '0.0.0.0.21.dev1+g68046e2.d20250420'
+__version_tuple__ = version_tuple = (0, 0, 0, 0, 21, 'dev1', 'g68046e2.d20250420')
